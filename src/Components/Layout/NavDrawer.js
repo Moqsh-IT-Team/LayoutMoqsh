3a3708cc02ad83631e17b1dbd9ec998412b303ef
Pratham Aggarwal <prathu.agg@gmail.com>
2021-06-24 16:25:14 +0530

    Basic SideDrawer

--- src/Components/Layout/NavDrawer.js
+++ src/Components/Layout/NavDrawer.js
@@ -3,8 +3,15 @@ import "./NavDrawer.css";
 const NavDrawer = (props) => {
   return (
     <div className="drawer">
-      <div className="left"></div>
-      <div className="Right"></div>
+      <h1> Events</h1>
+      <h3> Event1</h3>
+      <h3> Event2</h3>
+      <h3> Event3</h3>
+      <h3> Event4</h3>
+      <h1> Team</h1>
+      <h3> Organizing</h3>
+      <h3> Handling</h3>
+      <h3> On ground</h3>
     </div>
   );
 };
