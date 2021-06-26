d9144cd997a0ed7689e6eb53ae31b729dcf47a40
Pratham Aggarwal <prathu.agg@gmail.com>
2021-06-26 22:26:42 +0530

    Switching Branches

--- src/Views/LandingPage/Components/LandingSection/LandingSection.js
+++ src/Views/LandingPage/Components/LandingSection/LandingSection.js
@@ -1,7 +1,25 @@
 import "./LandingSection.scss";
-
+import logo from "../../../../Assets/Images/logo.jpg";
+import moqsh from "../../../../Assets/Images/Moqsh3.jpeg";
 function LandingSection() {
-  return <section id="LandingSection" className="LandingSection"></section>;
+  return (
+    <section id="LandingSection" className="LandingSection">
+      <div className="LandingPageBody">
+        <div className="LandingPageBody__img">
+          <img src={logo} alt="" />
+        </div>
+
+        <div className="LandingPageBody__text">
+          <div className="LandingPageBody__text__img">
+            <img src={moqsh} alt="" />
+          </div>
+          <h1 className="LandingPageBody__text__H1A">The Change </h1>
+          <h1 className="LandingPageBody__text__H1B">Starts With Us</h1>
+        </div>
+      </div>
+      );
+    </section>
+  );
 }
 
 export default LandingSection;
