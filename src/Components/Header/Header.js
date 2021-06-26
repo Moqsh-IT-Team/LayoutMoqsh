b186f7184a6b33f9f3c9bb55a6b219dc9fecb73e
Pratham Aggarwal <prathu.agg@gmail.com>
2021-06-26 23:09:52 +0530

    Basic Landing Layout Complete

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -27,24 +27,6 @@ function Header() {
           </div>
         </div>
       </div>
-
-      {/* <a href="/" className="logo">
-        Logo
-      </a>
-      <ul>
-        <li>
-          <a href="/">Home</a>
-        </li>
-        <li>
-          <a href="/team">Team</a>
-        </li>
-        <li>
-          <a href="/events">Events</a>
-        </li>
-        <li>
-          <a href="/sponsors">Sponsors</a>
-        </li>
-      </ul> */}
     </div>
   );
 }
