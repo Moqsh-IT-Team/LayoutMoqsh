5664e38dab5e4a8ed0a2a7a341698d61178589dc
unknown <goelesha2001@gmail.com>
2021-06-27 22:10:39 +0530

    Speakers Section Added

--- src/Views/LandingPage/Components/SpeakerSection/SpeakerSection.js
+++ src/Views/LandingPage/Components/SpeakerSection/SpeakerSection.js
@@ -1,12 +1,15 @@
 import SectionHeader from "../../../../Components/SectionHeading/SectionHeading";
 import "./SpeakerSection.scss";
+import Card1 from './Comp/Cards';
 
 function SpeakerSection() {
   return (
     <section id="SpeakerSection" className="SpeakerSection">
       <SectionHeader text="Our Speakers" />
+      <Card1/>
     </section>
   );
+
 }
 
 export default SpeakerSection;
