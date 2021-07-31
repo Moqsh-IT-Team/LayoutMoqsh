978d2982c65602f898c82e40c19580b30ebefb94
unknown <goelesha2001@gmail.com>
2021-07-31 17:41:31 +0530

    Added final sponsor logos

---
+++ src/Assets/Sponsors/index.js
@@ -0,0 +1,75 @@
+import file1 from "./aicsnu.jpg";
+import file2 from "./amd.png";
+import file3 from "./ansul.png"
+import file4 from "./arrow.jpg";
+import file5 from "./ascend.png";
+import file6 from "./atkt.png";
+import file7 from "./bankofbaroda.png";
+import file8 from "./bluestar.png";
+import file9 from "./boi.jpg";
+import file10 from "./bookmyevent.jpg";
+import file11 from "./canarabank.png";
+import file12 from "./canvas.png";
+import file13 from "./chaipeenihai.png";
+import file14 from "./cisco.png";
+import file15 from "./cocacola.png";
+import file16 from "./d2c.png";
+import file17 from "./daikin.png";
+import file18 from "./decathlon.png";
+import file19 from "./digitalgurukul.jpg";
+import file20 from "./dubeat.jpg";
+import file21 from "./ebay.png";
+import file22 from "./fortinet.png";
+import file23 from "./frapp.png";
+import file24 from "./fresca.jpg";
+import file25 from "./furball.jpg";
+import file26 from "./grabon.png";
+import file27 from "./hcl.png";
+import file28 from "./hdfcbank.png";
+import file29 from "./heico.png";
+import file30 from "./ics.jpg";
+import file31 from "./igl.png";
+import file32 from "./imperfecto.png";
+import file33 from "./inuth.jpg"
+import file34 from "./jio.png";
+import file35 from "./juniper.png";
+import file36 from "./logix.jpg";
+import file37 from "./logo.jpg";
+import file38 from "./lt.png";
+import file39 from "./mtv.png";
+import file40 from "./mycap.png";
+import file41 from "./mysun.png";
+import file42 from "./oneindia.png";
+import file43 from "./onoma.jpg";
+import file44 from "./oyo.jpg";
+import file45 from "./payu.png";
+import file46 from "./pepsi.png";
+import file47 from "./pidilite.png";
+import file48 from "./pnb.png";
+import file49 from "./proactive.png";
+import file50 from "./redbull.png";
+import file51 from "./royalenfield.jpg";
+import file52 from "./saavn.png";
+import file53 from "./sbi.png";
+import file54 from "./shopclues.png";
+import file55 from "./souledstore.png";
+import file56 from "./spykar.png";
+import file57 from "./statcraft.png";
+import file58 from "./tetronics.png";
+import file59 from "./toi.png";
+import file60 from "./unicos.png";
+import file61 from "./universityexpress.png";
+import file62 from "./varietybooks.png";
+import file63 from "./vlcc.png";
+import file64 from "./wildcraft.png";
+import file65 from "./wiley.png";
+import file66 from "./wwf.png";
+import file67 from "./zebronics.png";
+
+const images = {
+    file1,file2,file3,file4,file5,file6,file7,file8,file9,file10,file11,file12,file13,file14,file15,file16,file17,file18,file19,file20,
+    file21,file22,file23,file24,file25,file26,file27,file28,file29,file30,file31,file32,file33,file34,file35,file36,file37,file38,file39,
+    file40,file41,file42,file43,file44,file45,file46,file47,file48,file49,file50,file51,file52,file53,file54,file55,file56,file57,file58,
+    file59,file60,file61,file62,file63,file64,file65,file66,file67
+}
+export default images; 
