1978843d2c40c6483e6953aed869749398da073e
ABSanthosh <a.b.santhosh02@gmail.com>
2021-08-22 14:47:38 +0530

    Small space filler component in teams page

---
+++ src/Views/TeamsPage/Components/TeamHeader/TeamHeader.js
@@ -0,0 +1,18 @@
+import React from "react";
+// import PropTypes from "prop-types";
+import "./TeamHeader.scss";
+//import { Test } from './TeamHeader.styles';
+
+function TeamHeader({ props }) {
+  return <div className="TeamHeaderWrapper">Meet the Team.</div>;
+}
+
+TeamHeader.propTypes = {
+  // bla: PropTypes.string,
+};
+
+TeamHeader.defaultProps = {
+  // bla: 'test',
+};
+
+export default TeamHeader;
