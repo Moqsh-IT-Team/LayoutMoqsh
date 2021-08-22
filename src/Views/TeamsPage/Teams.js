ac9380a9589b332c4b33a3e1a7acc773a47ad27a
ABSanthosh <a.b.santhosh02@gmail.com>
2021-08-22 14:46:05 +0530

    New teams page

---
+++ src/Views/TeamsPage/Teams.js
@@ -0,0 +1,16 @@
+import Header from "../../Components/Header/Header";
+import Footer from "../../Components/Footer/Footer";
+import TeamHeader from "./Components/TeamHeader/TeamHeader";
+import TeamTabs from "./Components/TeamTabs/TeamTabs";
+function TeamsPage() {
+  return (
+    <>
+      <Header />
+      <TeamHeader />
+      <TeamTabs />
+      <Footer />
+    </>
+  );
+}
+
+export default TeamsPage;
